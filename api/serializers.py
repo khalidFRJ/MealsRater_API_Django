30dfe9f0face689e95cdee46c10d7339bd3695d1
khalid <elfarajkhalid525@gmail.com>
2024-09-06 10:28:43 +0100

    commit

--- api/serializers.py
+++ api/serializers.py
@@ -8,10 +8,9 @@ from .models import Meal, Rating
 class MealSerializer(serializers.ModelSerializer):
     class Meta:
         model = Meal
-        fields = ('id', 'title', 'description',)
+        fields = ('id', 'title', 'description')
 
 class RatingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rating
         fields = ('id', 'stars', 'user', 'meal')
-        
